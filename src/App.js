e5850e74f22bb212ef197be77fc02ca1d0c7bd4d
Adoy7 <markjosephlapera@gmail.com>
2024-11-07 12:28:34 +0800

    remove responsive
    Removing responsiveness to work again in the future. Focusing first on other high priority tasks.

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import Navbar from './Navbar.js'
 
 function App() {
 
-
   return (
     <div className="App">
         <Navbar />
+        <div className="blog-contents flex bg-red-500 h-screen w-full py-auto px-190">
+          <LeftContent />
+        </div>
         
     </div>
   );
--- src/Navbar.js
+++ src/Navbar.js
@@ -2,16 +2,12 @@ import './index.css'
 
 const Navbar = () => {
     return ( 
-        <nav className="navbar flex justify-between h-88 py-auto max-tablet:px-0 px-190 border-solid border-b-black border-b border-opacity-25">
+        <nav className="navbar flex justify-between h-88 py-auto px-190 border-solid border-b-black border-b border-opacity-25">
             <div className="nav-left flex my-auto">
                 <img src={`./assets/BalagBLOG!.svg`} alt="brand-icon" className='w-180 h-8 mr-10 my-auto' />
-                <div className="write-blog-m flex items-center gap-2 bg-white py-2 px-5 rounded-full tablet:hidden">
-                        <img src={"./assets/write-blog-icon.svg"} alt="write-blog-icon" />
-                        <p className="title">Write Blog</p>
-                    </div>
-                <div className="write-blog flex px-5 py-2 bg-white rounded-full max-tablet:hidden">
+                <div className="write-blog flex px-5 py-2 bg-white rounded-full ">
                     <img src={`./assets/8bit-me_blackbg.png`} alt="user-icon" className='my-auto size-35 bg-cover rounded-full' />
-                    <input type="text" name="write-blog" id="write-blog" placeholder="What's on your mind, Joseph?" className='max-laptop:w-auto w-548 my-auto mx-5 py-2 pl-5 bg-eggplant bg-opacity-10 rounded-full'/>
+                    <input type="text" name="write-blog" id="write-blog" placeholder="What's on your mind, Joseph?" className='w-548 my-auto mx-5 py-2 pl-5 bg-eggplant bg-opacity-10 rounded-full'/>
                 </div>
             </div>
             <div className="nav-right my-auto">
